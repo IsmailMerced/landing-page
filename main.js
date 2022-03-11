f6dd7fe3c7704b5900baaff68ac7e05f2ba1ee0b
Merced <77894409+ismailMerced@users.noreply.github.com>
2022-03-11 12:52:02 +0300

    update

--- main.js
+++
@@ -1,7 +0,0 @@
-import { CountUp } from './node_modules/countup.js/dist/countUp.js';
-
-window.onload = function() {
-    var countUp = new CountUp('num', 5000, {suffix: ' user'});
-    countUp.start();
-  }
-
